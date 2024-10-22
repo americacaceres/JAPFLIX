dca5ee9ae05ad088f0f222533d62290516e7fde3
america <americacaceres10@gmail.com>
2024-10-22 18:42:46 -0300

    .

--- js/JSON.js
+++ js/JSON.js
@@ -1,18 +1,24 @@
-const JsonMovies =  'https://japceibal.github.io/japflix_api/movies-data.json' ;
 
-function JsonData (url) {
+/*Cuando la página cargue, deberá traer el listado de información sobre películas 
+disponible en https://japceibal.github.io/japflix_api/movies-data.json, pero no mostrarlo al usuario. */
+let peliculas;
+
+const JsonMovies =  'https://japceibal.github.io/japflix_api/movies-data.json' ; // link del JSON
+
+function JsonData (url) { // función para traer los datos del JSON
 
     fetch(url) 
     .then (response => response.json())
     .then (data => {
-        console.log(data)
+        console.log(data) 
+        let peliculas = data
     })
     .catch (error => {
-    console.error('Error al obtener los datos', error); 
+    console.error('Error al obtener los datos', error); // por si ocurre un error al cargar los datos 
     })
 
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() { // evento que llama a la función una vez cargada la página
     JsonData(JsonMovies)
 } ) 
---
+++ js/search.js
@@ -0,0 +1,30 @@
+/*    
+Cuando el usuario presiona el botón buscar, y si previamente ingresó algún valor en el campo de búsqueda, 
+deberá mostrar un listado con las películas que coincidan con dicha búsqueda en sus atributos de title
+ o genres o tagline u overview. La información a mostrar en este punto será: title, tagline, y vote_average 
+ (en formato de "estrellas").
+ */
+let inputBuscar = document.getElementById('inputBuscar');
+
+ document.getElementById('btnBuscar').addEventListener('click', function () {
+    let fraseBuscada = inputBuscar.value.trim(); // obtiene la búsqueda del usuario 
+  if (fraseBuscada) {
+    BuscarPeliculas (); // buscar y mostrar resultados 
+  }
+
+ })
+
+ function BuscarPeliculas (peliculasArray, fraseBuscada ) {
+    const fraseBuscadaLower = fraseBuscada.toLowerCase();
+    let peliculasBuscadas = peliculasArray.filter (movie =>
+        movie.title.toLowerCase().includes(fraseBuscadaLower) ||
+        movie.genres.join(', ').toLowerCase().includes(fraseBuscadaLower) ||
+        movie.tagline.toLowerCase().includes(fraseBuscadaLower) ||
+        movie.overview.toLowerCase().includes(fraseBuscadaLower)
+     );
+       peliculasBuscadas.forEach(pelicula => {
+        
+    
+         });        
+    
+ }
